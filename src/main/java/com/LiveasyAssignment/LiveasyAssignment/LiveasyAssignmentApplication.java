9c08bc3588000f2ca3688d807650c0aa307b5114
deeksha058 <deekshajindal058@gmail.com>
2023-02-18 21:42:12 +0530

    Initial commit

---
+++ src/main/java/com/LiveasyAssignment/LiveasyAssignment/LiveasyAssignmentApplication.java
@@ -0,0 +1,14 @@
+package com.LiveasyAssignment.LiveasyAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveasyAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveasyAssignmentApplication.class, args);
+		System.out.println("Run Successfully! ");
+	}
+
+}
---
+++ src/test/java/com/LiveasyAssignment/LiveasyAssignment/LiveasyAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LiveasyAssignment.LiveasyAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveasyAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
